33d1b5e9adc694e8e71bf95e22024cd6520485c6
SIMRANKUMARI11 <simranrauniyar387@gmail.com>
2026-07-03 00:24:26 +0530

    Create PeakElement.java

---
+++ PeakElement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;   // move right
+            } else {
+                right = mid;      // move left (including mid)
+            }
+        }
+
+        return left; // or right (both same)
+    }
+}
